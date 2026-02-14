8dfa241029751477e49c417bba8969616a0948f0
John Murdoch <116120246+jlmurdoch@users.noreply.github.com>
2026-02-14 15:29:14 +0000

    Update epdconfig.py
    Update epdconfig.py to reflect new /sys gpiomem driver for Raspberry Pi

--- lib/waveshare_epd/epdconfig.py
+++ lib/waveshare_epd/epdconfig.py
@@ -148,7 +148,7 @@ class JetsonNano:
         self.GPIO.cleanup([self.RST_PIN, self.DC_PIN, self.CS_PIN, self.BUSY_PIN])
 
 
-if os.path.exists('/sys/bus/platform/drivers/gpiomem-bcm2835'):
+if os.path.exists('/sys/bus/platform/drivers/rpi-gpiomem'):
     implementation = RaspberryPi()
 else:
     implementation = JetsonNano()
